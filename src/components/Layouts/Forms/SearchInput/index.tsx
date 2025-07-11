c39eb072aa3704a876e0014ce9b92e74ff3b6976
Caahmos <dev.alexmorales@gmail.com>
2025-07-11 19:29:35 -0300

    feat: Close Book Search Drowpdown by Ref

--- src/components/Layouts/Forms/SearchInput/index.tsx
+++ src/components/Layouts/Forms/SearchInput/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import type { InputHTMLAttributes } from 'react';
-import { Container, StyledInputContainer, StyledInput, IconWrapper, Dropdown, CloseButton, SelectMethod } from "./styles";
+import React, { useEffect, useRef } from "react";
+import type { InputHTMLAttributes } from "react";
+import {
+    Container,
+    StyledInputContainer,
+    StyledInput,
+    IconWrapper,
+    Dropdown,
+    CloseButton,
+    SelectMethod,
+} from "./styles";
 import type { Biblio } from "../../../../model/Biblio/Biblio/SearchBiblioResponse";
 import BookSearch from "../../BookSearch";
 import { IoCloseSharp } from "react-icons/io5";
 import { useHandleSearch } from "../../../../hooks/useHandleSearch";
-
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
@@ -16,42 +23,87 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     onChangeMethod: (value: string) => void;
 }
 
-const SearchInput: React.FC<InputProps> = ({ icon, searchResults, isOpen, ...props }) => {
+const SearchInput: React.FC<InputProps> = ({
+    icon,
+    searchResults,
+    isOpen,
+    method,
+    onChangeMethod,
+    ...props
+}) => {
     const { close } = useHandleSearch();
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const clearInput = () => {
+        if (props.onChange) {
+            const event = {
+                target: { value: "" },
+            } as unknown as React.ChangeEvent<HTMLInputElement>;
+            props.onChange(event);
+        }
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                containerRef.current &&
+                !containerRef.current.contains(event.target as Node)
+            ) {
+                close();
+                clearInput();
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     return (
-        <Container>
+        <Container ref={containerRef}>
             <StyledInputContainer>
                 {icon && <IconWrapper>{icon}</IconWrapper>}
+
                 <StyledInput {...props} />
 
-                <SelectMethod value={props.method} onChange={(e) => props.onChangeMethod(e.target.value)}>
+                <SelectMethod
+                    value={method}
+                    onChange={(e) => onChangeMethod(e.target.value)}
+                >
                     <option value="title">Título</option>
                     <option value="author">Autor</option>
                     <option value="barcode">Código</option>
                 </SelectMethod>
 
-                <CloseButton onClick={() => close()}>
+                <CloseButton
+                    onClick={() => {
+                        close();
+                        clearInput();
+                    }}
+                >
                     <IoCloseSharp />
                 </CloseButton>
             </StyledInputContainer>
 
             <Dropdown $isopen={isOpen}>
-                {
-                    searchResults && searchResults.map((biblio) => (
+                {searchResults &&
+                    searchResults.map((biblio) => (
                         <BookSearch
                             key={biblio.bibid}
                             id={biblio.bibid}
                             title={biblio.title}
                             author={biblio.author}
-                            img={biblio.BiblioMedia && biblio.BiblioMedia[0]?.imageUrl || 'semcapa.png'}
-                            rank={biblio.BiblioMedia && biblio.BiblioMedia[0]?.rank || 0}
+                            img={
+                                biblio.BiblioMedia?.[0]?.imageUrl ||
+                                "semcapa.png"
+                            }
+                            rank={biblio.BiblioMedia?.[0]?.rank || 0}
                         />
-                    ))
-                }
+                    ))}
             </Dropdown>
         </Container>
     );
-}
+};
 
 export default SearchInput;

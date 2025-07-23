95aab18aa6d265a940926fa7b8b49c9a1a1de9e0
Caahmos <dev.alexmorales@gmail.com>
2025-07-23 10:38:06 -0300

    feat: Login page finished

--- src/components/Pages/Auth/Login/index.tsx
+++ src/components/Pages/Auth/Login/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LoginForm from "../../../Layouts/Forms/Auth/LoginForm";
-import { Container, Left, Right } from './styles';
+import { AppAuthor, Container, Content, Copyright, FooterContainer, GithubIcon, Left, Right, Sign } from './styles';
 import type { LoginStaffRequest } from "../../../../model/Staff/LoginStaffRequest";
 import { useAuth } from "../../../../hooks/useAuth";
 import OutlinedLogo from "../../../../assets/logo/OutlinedLogo";
+import students from '../../../../assets/imgs/students.png';
+import Logowhite from "../../../../assets/logo/Logowhite";
 
 const Login: React.FC = () => {
-    const {signIn} = useAuth();
+    const { signIn } = useAuth();
 
     const loginStaff = (data: LoginStaffRequest) => {
         signIn(data);
@@ -14,10 +16,20 @@ const Login: React.FC = () => {
 
     return (
         <Container>
-            <Left></Left>
+            <Left>
+                <img width={600} src={students} />
+                <FooterContainer>
+                    <Sign>
+                        <Logowhite />
+                        <Copyright>
+                            <span>©Todos os diretos reservados - </span><AppAuthor href='https://github.com/Caahmos'><GithubIcon /><span> Cauã Morales</span></AppAuthor>
+                        </Copyright>
+                    </Sign>
+                </FooterContainer>
+            </Left>
             <Right>
-                <LoginForm handleSubmit={loginStaff} button_text="Entrar"/>
-                <div style={{ position: "absolute", zIndex: 1}}><OutlinedLogo/></div>
+                <LoginForm handleSubmit={loginStaff} button_text="Entrar" />
+                <div style={{ position: "absolute", zIndex: 1 }}><OutlinedLogo /></div>
             </Right>
         </Container>
     );
--- src/components/Pages/Auth/Login/styles.ts
+++ src/components/Pages/Auth/Login/styles.ts
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -12,6 +13,10 @@ export const Container = styled.div`
 export const Left = styled.div`
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   @media screen and (max-width: 1000px) {
     display: none;
@@ -36,3 +41,57 @@ export const Right = styled.div`
   border-bottom-left-radius: 0px;
   }
 `;
+
+export const FooterContainer = styled.footer`
+    width: 80%;
+    color: ${ props => props.theme.colors.white };
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    z-index: 1;
+    `;
+
+export const Content = styled.div`
+    align-self: self-end;
+    flex-wrap: wrap;
+    width: 100%;
+    flex-grow: 1;
+    padding: 70px;
+    text-align: center;
+    background-color: ${ props => props.theme.colors.secondary };
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Sign = styled.div`
+    flex-wrap: wrap;
+    width: 100%;
+    padding: 20px 40px;
+    background-color: ${(props) => props.theme.colors.secondary};
+    display: flex;
+    border-radius: 70px;
+    justify-content: space-between;
+`
+
+export const GithubIcon = styled(FaGithub)`
+    font-size: 1rem;
+    color: ${ props => props.theme.colors.success };
+    `
+export const Copyright = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`
+
+export const AppAuthor = styled.a`
+    display: flex;
+    color: ${ props => props.theme.colors.white };
+    align-items: center;
+    text-decoration: none;
+    gap: 5px;
+    transition: 0.4s all ease;
+    
+    &:hover{
+        color: ${ props => props.theme.colors.success };
+    }
+`
